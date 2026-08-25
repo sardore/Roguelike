37bfd167b191d12546c23168cea183a49f4bfc82
sardore <166273766+sardore@users.noreply.github.com>
2026-08-26 07:56:13 +0900

    Zoom the live dungeon camera out without shrinking the mobile UI

---
+++ src/main4.ts
@@ -0,0 +1,16 @@
+const scale=1.28;
+const widthDesc=Object.getOwnPropertyDescriptor(HTMLCanvasElement.prototype,'width');
+const heightDesc=Object.getOwnPropertyDescriptor(HTMLCanvasElement.prototype,'height');
+if(widthDesc?.get&&widthDesc.set&&heightDesc?.get&&heightDesc.set){
+  Object.defineProperty(HTMLCanvasElement.prototype,'width',{
+    configurable:true,
+    get(){return widthDesc.get!.call(this)},
+    set(value:number){widthDesc.set!.call(this,this.id==='game'?Math.floor(value*scale):value)}
+  });
+  Object.defineProperty(HTMLCanvasElement.prototype,'height',{
+    configurable:true,
+    get(){return heightDesc.get!.call(this)},
+    set(value:number){heightDesc.set!.call(this,this.id==='game'?Math.floor(value*scale):value)}
+  });
+}
+void import('./main3');
